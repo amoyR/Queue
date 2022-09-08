d6b5492e3d44144d4eeff0027054257c290d54c2
Ryoma <amoyr@RyomanoMacBook-Air.local>
2022-09-08 13:18:51 +0900

    fixed add dummy head

--- queue.c
+++ queue.c
@@ -12,27 +12,35 @@ typedef struct
 typedef struct linked_list_tag
 {
   struct linked_list_tag *next;
-  purchase_data *item; 
+  purchase_data *log; 
 } linked_list;
 
 
-int init (linked_list **head, purchase_data *head_purchase_data)
+int init (linked_list **head)
 {
   *head = (linked_list*)malloc(sizeof(linked_list));
   (*head)->next = NULL;
-  (*head)->item = head_purchase_data;
+  (*head)->log = NULL;
 
   return 0;
 }
 
+int finalize (linked_list **head)
+{
+  free((*head)->log);
+  free(*head);
+
+  *head = NULL;
+
+  return 0;
+}
 
 linked_list *fetch_tail (linked_list *head)
 {
   linked_list *tail;
-  for (tail = head; ; tail = tail->next) {
-    if (tail->next == NULL) {
-      break;
-    }
+  tail = head;
+  while(tail->next != NULL) {
+    tail = tail->next;
   }
 
   return tail;
@@ -54,7 +62,7 @@ int enqueue (linked_list *head, purchase_data *new_purchase_data)
   linked_list *new_tail;
   new_tail = (linked_list*)malloc(sizeof(linked_list));
   new_tail->next = NULL;
-  new_tail->item = new_purchase_data;
+  new_tail->log = new_purchase_data;
 
   linked_list *pre_tail;
   pre_tail = fetch_tail(head);
@@ -63,44 +71,36 @@ int enqueue (linked_list *head, purchase_data *new_purchase_data)
   return 0;
 }
 
-int dequeue (linked_list **head, purchase_data *dequeued_data) 
+purchase_data *dequeue (linked_list *head, purchase_data *dequeued_data) 
 {
-  if (*head == NULL) {
-    return 0;
+  linked_list *queue_head;
+  queue_head = head->next;
 
-  } else if ((*head)->next == NULL) {
-    dequeued_data = (*head)->item;
-    free((*head)->item);
-    free(*head);
+  if (queue_head != NULL) {
+    dequeued_data = queue_head->log;
+    head->next = queue_head->next;
+    free(queue_head->log);
+    free(queue_head);
 
-    *head = NULL;
-
-    return 0;
   } else {
-    linked_list *pre_head;
-    pre_head = *head;
-    dequeued_data = pre_head->item;
-    *head = pre_head->next;
-
-    free(pre_head->item);
-    free(pre_head);
-
-    return 0;
+    dequeued_data = NULL;
   }
 
+  return dequeued_data;
 }
 
 int print_queue (linked_list *head)
 {
-  if (head == NULL) {
-    printf("queue is empty\n");
-  } else {
-    for (linked_list *current_node = head; ; current_node = current_node->next) {
-      printf("id -> %d price -> %d\n", current_node->item->id, current_node->item->price);
-      if (current_node->next == NULL) {
-        break;
-      }
+  linked_list *current_node;
+  current_node = head->next;
+  if (current_node != NULL) {
+    while(current_node->next != NULL) {
+      printf("id -> %d price -> %d\n", current_node->log->id, current_node->log->price);
+      current_node = current_node->next;
     }
+    printf("id -> %d price -> %d\n", current_node->log->id, current_node->log->price);
+  } else {
+    printf("queue is empty\n");
   }
 
   return 0;
@@ -109,24 +109,24 @@ int print_queue (linked_list *head)
 
 int main (void) 
 {
-  linked_list *head = NULL;
-  purchase_data *init_purchase_data; 
-  purchase_data dequeued_data; 
+  linked_list *head;
+  purchase_data *dequeued_data; 
 
-  init_purchase_data = create_purchase_data(100, 100);
-  init(&head, init_purchase_data);
+  init(&head);
 
   purchase_data *add_data;
   add_data = create_purchase_data(200, 200);
   enqueue(head, add_data);
-
-  dequeue(&head, &dequeued_data);
-  dequeue(&head, &dequeued_data);
-  dequeue(&head, &dequeued_data);
-  dequeue(&head, &dequeued_data);
   print_queue(head);
 
-
+  dequeue(head, dequeued_data);
+  print_queue(head);
+  dequeue(head, dequeued_data);
+  dequeue(head, dequeued_data);
+  dequeue(head, dequeued_data);
+  print_queue(head);
+  
+  finalize(&head);
   return 0;
 }
 
